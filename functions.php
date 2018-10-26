0f483c29efadfbf2184ada4b3012bd2a67542707
robert hunnicutt <robhunn74@gmail.com>
2018-10-26 06:09:54 -0500

    disable css link in header add function.php to handle life cycle events

--- functions.php
+++ functions.php
@@ -6,12 +6,13 @@
 function phpforwp_theme_styles() {
 
   // Enqueue google fonts https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round
+  wp_enqueue_style('font-css', 'https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round' );
   // Enque main style sheet (make dependent on google fonts)
-
+  wp_enqueue_style('main-css', get_stylesheet_uri(), ['font-css'], get_the_time() );
 }
 // Add phpforwp_theme_styles function to wp_enqueue_scripts action hook
 // with a priority of 10
-add_action( '', '', 0 );
+add_action( 'wp_enqueue_scripts', 'phpforwp_theme_styles', 10 );
 
 
 /**
@@ -24,15 +25,17 @@ function phpforwp_read_more_link( $excerpt ) {
 
   // Create a variable called $extended_excerpt and
   // assign it the value of $excerpt
-
+  $extended_excerpt = $excerpt;
   // Append a read more link using get_permalink() as the url
-
+  $extended_excerpt .= ' <a href=" ' . get_permalink() . ' "> ';
+  $extended_excerpt .= 'Read more &raquo;';
+  $extended_excerpt .= '</a>';
   // Return $extended_excerpt
 
 }
 // Add phpforwp_read_more_link function to the get_the_excerpt
 // with a priority of 10
-add_filter( '', '', 0 );
+add_filter( 'get_the_excerpt', 'phpforwp_read_more_link', 10 );
 
 
 ?>
--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>PHP for WordPress</title>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css"> -->
   <?php wp_head(); ?>
 </head>
 <body class="<?php body_class(); ?>">
